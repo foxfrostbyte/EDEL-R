4da2e37579235b9280480dbea468e8bd0b3ae218
Elena N.J. <98986354+h669798@users.noreply.github.com>
2024-12-22 02:53:11 +0100

    Oppdatering til navigasjon

---
+++ src/main/resources/static/scripts/index.js
@@ -0,0 +1,9 @@
+// Viser "overlay" når meny-knapp blir trykket
+document.querySelector(".enterNavigasjon").addEventListener("click", () => {
+    document.querySelector(".navigasjon").style.display="block";
+});
+
+// Fjerner "overlay" og går tilbake til hjemmeside
+document.querySelector(".exitNavigasjon").addEventListener("click", () => {
+   document.querySelector(".navigasjon").style.display="none";
+});
